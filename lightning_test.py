ecd60518e2d46f767bb924597113e57726cee9da
itruonghai <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>
2022-04-28 00:54:51 +0700

    add visualize

--- lightning_test.py
+++ lightning_test.py
@@ -1,4 +1,4 @@
-# from data.brats import get_train_dataloader, get_val_dataloader, get_test_dataloader
+from data.brats import get_train_dataloader, get_val_dataloader, get_test_dataloader
 import pytorch_lightning as pl
 from trainer import BRATS
 import os 
@@ -7,11 +7,11 @@ os.system('cls||clear')
 print("Testing ...")
 
 CKPT = 'best.ckpt'
-model = BRATS().load_from_checkpoint(CKPT).eval()
-# val_dataloader = get_val_dataloader()
-# test_dataloader = get_test_dataloader()
-# trainer = pl.Trainer(gpus = [0], precision=32, progress_bar_refresh_rate=10)
+model = BRATS(use_VAE=True).load_from_checkpoint(CKPT).eval()
+val_dataloader = get_val_dataloader()
+test_dataloader = get_test_dataloader()
+trainer = pl.Trainer(gpus = [0], precision=32, progress_bar_refresh_rate=10)
 
-# trainer.test(model, dataloaders = val_dataloader)
-# trainer.test(model, dataloaders = test_dataloader)
+trainer.test(model, dataloaders = val_dataloader)
+trainer.test(model, dataloaders = test_dataloader)
 
